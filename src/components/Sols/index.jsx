dddc24488927d9f715b9a4a464075c53c45cc9e3
remibcn <118737122+remibcn@users.noreply.github.com>
2023-08-22 00:39:42 +0200

    ok c pas mal

--- src/components/Sols/index.jsx
+++ src/components/Sols/index.jsx
@@ -1,7 +1,6 @@
 import image1 from '../../assets/palette.jpeg';
 import image2 from '../../assets/palette.jpeg';
 import './styles.scss';
-import SolsProductsList from './SolsProductsList';
 
 function Sols() {
   const ImagesSols = [
@@ -9,29 +8,25 @@ function Sols() {
       id: 1,
       src: image1,
       alt: 'Image1',
-      caca: 'test',
+
     },
     {
       id: 2,
       src: image2,
       alt: 'Image2',
-      text: 'text',
     },
   ];
 
-  let listOfProducts;
-  if (ImagesSols) {
-    listOfProducts = ImagesSols.map((image) => (
-      <SolsProductsList key={image.id} image={image} />
-    ));
-  }
+  // let listOfProducts;
+  // if (ImagesSols) {
+  //   listOfProducts = ImagesSols.map((image) => (
+  //     <SolsProductsList key={image.id} image={image} />
+  //   ));
+  // }
   return (
     <div className="container">
       <div className="column">
-        <div className="column-image">
-          {ImagesSols
-          && listOfProducts}
-        </div>
+        <img src={image1} alt="test" />
       </div>
     </div>
   );
